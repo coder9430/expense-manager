f6a812d3cc91e296f8cb60608a71ab4a781c891a
coder9430 <cse21bcsc01@silicon.ac.in>
2024-05-11 11:47:11 +0530

    graph completed

---
+++ frontend/src/Fake_data2.js
@@ -0,0 +1,7 @@
+export const pieData = {
+    label: ["Expenditure", "Savings"],
+    data: [200, 500] // Dummy values representing expenditure and savings/investment
+  };
+  //yaha
+  // ok let me see ok are u there? yes
+  
